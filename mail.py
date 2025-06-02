ef5b7125c9f9981b609b34c931c9661a2635c3ea
Arcadievich <user_274@mail.ru>
2025-06-02 15:43:15 +0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -9,19 +9,21 @@ password = os.getenv('MAIL_APP_PASSWORD')
 website = 'https://dvmn.org/profession-ref-program/dmitry_274/VoB3j/'
 friend_name = 'Лёха'
 my_name = 'Димон'
+sender = 'dimotron1999@yandex.ru'
+recipient = 'dimotron1999@yandex.ru'
 
-letter = ("""\
-From: dimotron1999@yandex.ru
-To: dimotron1999@yandex.ru
+letter = """\
+From: {0}
+To: {1}
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";
 
-Привет, {0}! {1} приглашает тебя на сайт {2}!
+Привет, {2}! {3} приглашает тебя на сайт {4}!
 
-{2} — это новая версия онлайн-курса по программированию. 
+{4} — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
 
-Как будет проходить ваше обучение на {2}? 
+Как будет проходить ваше обучение на {4}? 
 
 → Попрактикуешься на реальных кейсах. 
 Задачи от тимлидов со стажем от 10 лет в программировании.
@@ -30,11 +32,11 @@ Content-Type: text/plain; charset="UTF-8";
 → Подготовишь крепкое резюме.
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
-Регистрируйся → {2}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""").format(friend_name, my_name, website)
+Регистрируйся → {4}  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".format(sender, recipient, friend_name, my_name, website)
 
 message = letter.encode("UTF-8")
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
 server.login(login, password)
-server.sendmail('dimotron1999@yandex.ru', 'dimotron1999@yandex.ru', message)
+server.sendmail(sender, recipient, message)
 server.quit()
